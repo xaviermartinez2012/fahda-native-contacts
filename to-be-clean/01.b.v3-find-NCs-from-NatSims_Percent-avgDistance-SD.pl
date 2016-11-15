d763a1560f2a5349a92adada817cfeffced6342d
khaister <nguyenkhai101@gmail.com>
2016-11-14 22:09:38 -0800

    Added scripts from sorin2

---
+++ to-be-clean/01.b.v3-find-NCs-from-NatSims_Percent-avgDistance-SD.pl
@@ -0,0 +1,200 @@
+#!/usr/bin/perl
+use POSIX qw/strftime/;
+use Statistics::Descriptive;
+use FileHandle;
+STDOUT->autoflush(1); # flush anything in buffer to output to avoid delayed outputing
+
+# ========================================================================================
+#   KHAI K.Q. NGUYEN
+#   California State University, Long Beach
+#   Biochemistry, 2014
+# ========================================================================================
+
+
+ 
+# =======================================================================================
+# SCRIPT INFO
+	$scriptInfo = "This script calculates the percent a native contact appears and its average distance.\n
+	./01.b-find-NCs-from-NatSims.pl \[0\] \[1\] \[2\]\n
+	Option     Filename       Type           Description\n
+	-------------------------------------------------------------------------------------\n
+	0.         contacts.txt   Input          List of contacts from native simulations\n
+	1.         natsims.txt    Input          List of native simulations, used to find the number of frames\n 
+											   (contains project, run, clone, time)\n
+	2.         output.txt     Output         Textfile ______________\n
+	\n
+	Run the script with \"h\" or\"help\" argument to display this message.\n";
+
+	$usage = "\$perl script.pl [nat-sims-contacts]  [native-sims-list]  [output]\n";
+
+
+# =======================================================================================
+# GET VALUES FROM USER INPUT
+	$contactsList = $ARGV[0] or die "$usage\n";
+	$nativeSims   = $ARGV[1] or die "$usage\n";
+	$outputFile   = $ARGV[2] or die "$usage\n";
+
+	if (($ARGV[0] eq "h") or ($ARGV[0] eq "help")) { print $usage; exit(); }
+
+	# Print input arguments & start time
+	print "$0 "; foreach my $item (@ARGV) { print "$item "; } print "\n";
+	$timeStart = strftime('%Y-%m-%d-%H-%M-%S',localtime);
+	print "Script starts at: $timeStart.\n";
+
+
+# =======================================================================================
+# CALCULATING THE NUMBER OF FRAMES
+	open (NATSIMS, "<$nativeSims");
+
+	print "Calculating the total number of frames...\n";
+	# Get the last column only because it contains simulation time from all simulations.
+	@totalTime = ();
+	while (my $line = <NATSIMS>) {
+		chomp($line);
+		if ($line =~ m/#/) { next; } # skip comments
+		foreach ($line) { s/^\s+//; s/\s+$//; s/\s+/ /g;}
+		my @splitLine = split(/\s+/,$line);
+		push (@totalTime,@splitLine[scalar(@splitLine)-1]);
+	}
+
+	# Caculate total simulation time...
+	$totalTime = 0;
+	foreach my $item (@totalTime) {
+		$totalTime = $totalTime + $item;
+	}
+
+	# Each frame is recorded every 100ps. We don't take into account the frame0 for each simulation.
+	# So the number of frames = (total sim time)/100
+	$numFrames = ($totalTime/100);
+	print "Number of frames is $numFrames\n";
+
+	close NATSIMS;
+
+
+# =======================================================================================
+# COUNTING NUMBER OF INSTANCES A CONTACT APPEARS
+	$prevLine = "";
+	$line     = "";
+	# the above two variables are always conjecutive lines from the input (native sims contacts list)
+
+	$count       = 0;
+	$percent     = 0;
+	$meanDist    = 0;
+	$std         = 0;
+	$meanDist2SD = 0;
+
+	# Get current time to be used as prefix for temporary files which store distance(s) for an atoms pair
+	$currentTime   = strftime('%Y-%m-%d-%H-%M-%S',localtime);
+	$fileTimeStamp = "$currentTime"."-01b-temp-";
+	print "The prefixed \"$fileTimeStamp\" will be used for temparory files.\n";
+
+	open (CON,    "<$contactsList")  or die ("Could not open native simulations contacts file.\n");
+	open (OUTPUT, ">$outputFile")   or die ("Could not write to output file.\n");
+
+	while ($line = <CON>) {
+		chomp ($line);
+		$distance = substr($line,-5); # extract the distance
+		$line =~ s/.....$//; ### remove the distance--which is 5-character long--at the end of a line
+
+		if ($prevLine eq "") {  #(1) # $prevLine is an empty string ONLY at the beginning of the loop
+			print "reading the first line...\n\n";
+			$prevLine = $line;
+			
+			#writing the distance to a temp file
+			$file = "$fileTimeStamp"."$line".".txt";
+			open (DISTANCE, ">>$file");
+			print DISTANCE "$distance\n";
+			close DISTANCE;
+			next; 
+		}             
+
+		elsif ($line eq $prevLine) { #(2)  # if $prevLine is not a null string, then...
+			$file = "$fileTimeStamp"."$line".".txt";
+			open (DISTANCE, ">>$file");
+			print DISTANCE "$distance\n";
+			close DISTANCE;
+			next;
+		}
+
+		elsif ($line ne $prevLine) { # if $line isn't idential to $prevLine
+			$file = "$fileTimeStamp"."$line".".txt";
+			open (DISTANCE, ">>$file");
+			print DISTANCE "$distance\n";
+			close DISTANCE;
+
+			# count the number of distances per unique contact and get descriptive stat
+			
+			$file = "$fileTimeStamp"."$prevLine".".txt";
+			open (DISTANCE, "<$file");
+			my @distanceArray = ();
+			while (my $di = <DISTANCE>) { chomp($di);  push(@distanceArray,$di); }
+			close DISTANCE;
+
+			my $stat     = Statistics::Descriptive::Full -> new();
+			$stat -> add_data(@distanceArray);
+			$meanDist    = $stat -> mean();
+			$count       = $stat -> count();
+			$std         = $stat -> standard_deviation();
+			$meanDist2SD = $meanDist + 2 * $std;
+			$percent     = ($count/$numFrames) * 100;
+
+			if ($percent < $P) { # if the percentage is smaller than the cut-off $P (user-input)...
+				$prevLine = $line;
+				next;
+			}
+
+			else {
+				print "Writing to output file...\n\n";
+				$printLine = $prevLine;
+				$printLine =~ s/\s+/\t/g;
+				printf OUTPUT "$printLine"."%6.3f\t%6.3f\t%6.10f\t%6.10f\n", $percent, $meanDist, $std, $meanDist2SD;
+
+				$prevLine = $line;
+				next;
+			}
+		} #end of the last 'elsif'
+	} #end of 'while' loop
+
+	# Write the last contact...
+	# Why: The last set of identical lines will not be written because of condition (2)
+
+	$file = "$fileTimeStamp"."$prevLine".".txt";
+	open (DISTANCE, "<$file");
+	my @distanceArray = ();
+	while (my $di = <DISTANCE>) { chomp($di);  push(@distanceArray,$di); }
+	close DISTANCE;
+
+	my $stat     = Statistics::Descriptive::Full -> new();
+	$stat -> add_data(@distanceArray);
+	$meanDist    = $stat -> mean();
+	$count       = $stat -> count();
+	$std         = $stat -> standard_deviation();
+	$meanDist2SD = $meanDist + 2 * $std;
+	$percent     = ($count/$numFrames) * 100;
+
+	print "Writing to output file...\n\n";
+	$printLine = $prevLine;
+	$printLine =~ s/\s+/\t/g;
+	printf OUTPUT "$printLine"."%6.3f\t%6.3f\t%6.10f\t%6.10f\n", $percent, $meanDist, $std, $meanDist2SD;
+
+	close OUTPUT;
+	close CON;
+
+
+# =======================================================================================
+# MOVING TEMP FILES TO A TEMP DIRECTORY TO BE DELETED LATER BY USER
+	@tempFiles = `tree -Li 1 | grep $currentTime`;
+	$tempDir = "$currentTime-01b-temp-files";
+	`mkdir $tempDir`;
+
+	print "Moving temporary files to a $tempDir...\n";
+	foreach $item (@tempFiles) {
+		chomp $item;
+		`mv \"$item\" $tempDir\/`;
+	}
+	print "All temporary files have been moved to ./$tempDir.\n";
+	print "The output file is $outputFile.\n";
+# =======================================================================================
+
+$timeEnd = strftime('%Y-%m-%d-%H-%M-%S',localtime);
+print "Script ends at $timeEnd\n";
---
+++ to-be-clean/03.summarize-all-contact-data.pl
@@ -0,0 +1,234 @@
+#!/usr/bin/perl -w
+use POSIX qw/strftime/;
+use FileHandle;
+STDOUT->autoflush(1); # flush anything in buffer to output to avoid delayed outputing
+
+# ================================================================================================
+#  Summing up categorized native contacts for entire project per time stamp    By: Arad & Khai 1/13/14
+# ================================================================================================
+
+
+#`echo "$0 starts at $timeStart" | mail -s "server at sorin2 message" nguyenkhai101\@gmail.com`;
+
+# ================================================================================================
+# INPUT
+	$usage = "\nUsage: perl script\.pl [project] [nat-con-file] [all-contact-file] [percent] [distance] [distanceNC] [output-file] [exclude-list-output-file] [native-contacts-list-output-file]\n";
+	$proj            = $ARGV[0] || die "$usage\n";
+	$ncFile          = $ARGV[1] || die "$usage\n";
+	$allConFile      = $ARGV[2] || die "$usage\n";
+	$cutOffPercent   = $ARGV[3] || die "$usage\n";
+	$distance        = $ARGV[4] || die "$usage\n";
+	$distanceNC      = $ARGV[5] || die "$usage\n";
+	$outputFile      = $ARGV[6] || die "$usage\n";
+	$excludeListFile = $ARGV[7] || die "$usage\n";
+	$nativeConList   = $ARGV[8] || die "$usage\n";
+
+	# print entered command with arguments used and start time
+	print "$0 ";   foreach my $item (@ARGV) {print "$item ";}   print "\n";
+	$timeStart = strftime('%Y-%m-%d-%H-%M-%S',localtime);
+	print "Script starts at: $timeStart.\n";
+# ================================================================================================
+
+
+# ================================================================================================
+# INITIALIZE HASH TABLES
+	$numAtom       = 839; # number of atoms there are for the molecule, must be changed for every new molecule
+	for($i=1; $i < $numAtom; $i++){
+		for ($j=1; $j < $numAtom ; $j++){
+			$nativeSim{"$i:$j"} = 0;
+			$meanDista{"$i:$j"} = 0;
+			$mean2SDev{"$i:$j"} = 0;
+			$percent{"$i:$j"}   = 0;
+			$secStruc{"$i:$j"}  = "";
+			$exlu{"$i:$j"}      = 0;
+		}
+	}
+# ================================================================================================
+
+
+# ================================================================================================
+# OPENING AND STORING NATIVE SIMULATIONS CONTACTS INFO INTO HASH TABLES FOR LATER COMPARISON
+	open (outNAT, ">$nativeConList")   || die "Could not generate native contacts list.\n";
+	open (outEXL, ">$excludeListFile") || die "Could not generate excluded contacts list.\n";
+	open (inNCLIST, "<$ncFile")       || die "Could not open the native contact file correctly.\n";
+	
+	$line1         = 0;	
+	@line1         = ();
+	$nativeAtom1   = 0;
+	$nativeAtom2   = 0;
+	while ($line1 = <inNCLIST>) {
+		chomp($line1);
+		# removes white spaces beginning and end of a line,
+		# replaces any white space between words by a single white space.
+		foreach($line1) { s/^\s+//; s/\s+$//; s/\s+/ /g; }
+		@line1        = split(/ /,$line1);
+		
+		$nativeAtom1 = $line1[0];
+		$nativeAtom2 = $line1[4];
+
+		$nativeSim{"$nativeAtom1:$nativeAtom2"} = 1;
+		$percent{"$nativeAtom1:$nativeAtom2"}   = $line1[9];   #= $ncPercent;
+		$meanDista{"$nativeAtom1:$nativeAtom2"} = $line1[10];  #= $ncMeanDist;
+		$mean2SDev{"$nativeAtom1:$nativeAtom2"} = $line1[12];  #= $mean2SD;
+		$secStruc{"$nativeAtom1:$nativeAtom2"}  = $line1[13];  #= $secStru;
+
+		###### Printing out exluded contacts for reference
+		# if the percent is small, print line to exclusion list
+		if (($line1[9] < $cutOffPercent) or ($line1[12] > $distanceNC)) {
+			$exlu{"$nativeAtom1:$nativeAtom2"} = 1;
+			print outEXL "$line1\n";
+		}
+		else{
+			print outNAT "$line1\n";
+		}
+	} # end of `while` loop
+
+	close(inNCLIST);
+	close(outNAT);
+	close(outEXL);
+# ================================================================================================
+
+
+# ================================================================================================
+# OPENING OUTPUT FILE
+	open(OUT,   ">$outputFile") || die "Could not open output file correctly. $!\n";
+	open(inCON,"<$allConFile") || die "Could not open all contact file correctly. $!\n";
+
+	$count         = 0;
+	$line2         = 0;
+	@line2         = ();
+	@line2Char     = ();
+	$run           = 0;
+	$lastRun       = 0;
+	$clone         = 0;
+	$lastClone     = 0;
+	$time          = 0;
+	$lastTime      = 0;
+	$shouldWePrint = 0;
+	$prevClone     = 0;
+	$dist          = 0; # distance variable
+
+	# number of secondary structures (i.e. how many contacts in stem 1, stem 2, loop 1, loop 2)
+	$s1            = 0;
+	$s2            = 0;
+	$l1            = 0;
+	$l2            = 0;
+	$t             = 0;
+
+	$totNC         = 0;
+	$nonNC         = 0;
+# ================================================================================================
+
+
+# ================================================================================================
+# EXTRACTING INFORMATION ABOUT PROJ, RUN, CLONE, AND TIME
+	while ($line2 = <inCON>){
+		chomp($line2);
+		foreach($line2) { s/^\s+//;s/\s+$//; s/\s+/ /g; }
+		@line2     = split(/ /,$line2);
+		@line2Char = split(//,$line2); # split line into characters in order to detect c3eyhange in frame
+
+		$charIndex = scalar(@line2Char); # number of characters in line
+		if ($line2Char[0] eq "p"){
+			if ($count > 1){ 
+				$shouldWePrint = 1;
+				$lastRun       = $run;
+				$lastClone     = $clone;
+				$lastTime      = $time;
+			}
+
+			# No need to extract project number because it is input by user.
+			# An example of timestamp in all-contact-data file: p1796_r0_c0_f0.con
+
+			# extract run number
+			$run = "$line2Char[7]";
+			for ($j=8; $j<$charIndex; $j++){
+				if ($line2Char[$j] eq "_"){
+					$cstart = $j+2;   # position for clone number, see next code block below
+					last;
+				}
+				else{ $run = "$run"."$line2Char[$j]"; }
+			}
+
+			# extract clone number
+			$clone = "$line2Char[$cstart]";
+			$cstart++;
+			for ($j=$cstart; $j<$charIndex; $j++){
+				if ($line2Char[$j] eq "_"){
+					$fstart = $j+2;   # position for frame/time number, see next code block below
+					last;
+				}
+				else { $clone = "$clone"."$line2Char[$j]"; }
+			}
+
+			# extract frame number
+			$time = "$line2Char[$fstart]";
+			$fstart++;
+			for ($j=$fstart;$j<$charIndex;$j++){
+				if ($line2Char[$j] eq "."){
+					last;
+				}
+				$time = "$time"."$line2Char[$j]";
+			}
+
+			# frame number time 100 to get time in picoseconds
+			$time = $time*100;
+
+			if ($clone != $prevClone){
+				print "$proj $run $prevClone\n";
+				$prevClone = $clone;
+			}
+		} # end of `if ($line2Char[0] eq "p"){`
+# ================================================================================================
+
+
+# ================================================================================================
+# STARTING TO EVALUATE CONTACTS
+	$numColumn = scalar(@line2);
+
+	if ($numColumn == 10) {   # to check if the input concatenated all-contact data file is valid
+		$atom1 = $line2[0];
+		$atom2 = $line2[4];
+		$dist  = $line2[9];
+
+		##### Checkng and tabulating contact information for each simulation
+		if ($dist <= $distance) {
+			# if the atoms pair is on native list but not on exclusion list
+			if ($nativeSim{"$atom1:$atom2"} == 1 && $exlu{"$atom1:$atom2"} == 0){
+				if ($dist <= $mean2SDev{"$atom1:$atom2"}){
+					if ($secStruc{"$atom1:$atom2"}    eq "S1"){   $s1++;   }
+					elsif ($secStruc{"$atom1:$atom2"} eq "S2"){   $s2++;   }
+					elsif ($secStruc{"$atom1:$atom2"} eq "L1"){   $l1++;   }
+					elsif ($secStruc{"$atom1:$atom2"} eq "L2"){   $l2++;   }
+					elsif ($secStruc{"$atom1:$atom2"} eq "T" ){   $t++;    }
+				}
+			}
+			elsif ($nativeSim{"$atom1:$atom2"} == 0 && $exlu{"$atom1:$atom2"} == 0){
+				$nonNC++;
+			}
+		}
+	}
+
+	####  Printing to output file
+	if ($shouldWePrint==1){
+		$totNC = $s1 + $s2 + $l1 + $l2 + $t;
+		printf OUT "%5d\t%5d\t%5d\t%5d\t%5d\t%5d\t%5d\t%5d\t%5d\t%5d\t%5d\n", 
+				   $proj, $lastRun, $lastClone, $lastTime, $s1, $s2, $l1, $l2, $t, $totNC, $nonNC;
+		$s1=0; $s2=0; $l1=0; $l2=0; $t=0; $totNC=0; $nonNC=0; # reseting counters for next time frame
+		$shouldWePrint=0;
+	}
+	$count++;
+} # end of `while` loop
+
+$lastTime = $lastTime + 100;
+$totNC = $s1 + $s2 + $l1 + $l2 + $t;
+printf OUT "%5d\t%5d\t%5d\t%5d\t%5d\t%5d\t%5d\t%5d\t%5d\t%5d\t%5d\n", 
+		   $proj, $lastRun, $lastClone, $lastTime, $s1, $s2, $l1, $l2, $t, $totNC, $nonNC;
+
+close(OUT);
+close(inCON);
+
+$timeEnd = strftime('%Y-%m-%d-%H-%M-%S',localtime);
+print "Script ends at $timeEnd\n";
+#`echo "$0 starts at $timeEnd" | mail -s "server at sorin2 message" nguyenkhai101\@gmail.com`;
+# ================================================================================================
