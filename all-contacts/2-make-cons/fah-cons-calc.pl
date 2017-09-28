5b7c64d127686a1099a0f66808375f3280b0b4c7
Khai Nguyen <khai.nguyen@outlook.com>
2017-09-27 21:04:09 -0700

    Use perl's function for traversing the PROJ* tree
    * Rename variables to better show their purposes
    * Add comment on pdb data labels

--- all-contacts/2-make-cons/fah-cons-calc.pl
+++ all-contacts/2-make-cons/fah-cons-calc.pl
@@ -2,128 +2,152 @@
 
 use strict;
 use warnings;
+use Cwd;
 use Getopt::Long qw(HelpMessage);
 
 GetOptions("help|h" => sub { print STDOUT HelpMessage() });
 
 # 7.0 A or less atomic seperations will be recorded
-my $MAX_DISTANCE = 7.0;
+# my $MAX_DISTANCE = 7.0;
 
-# Delta(res) = res(j) - res(i) >= 3, must have 2 or more residues between
+# Delta(res) = res(j) - res(i) = the number of residues between atoms i's and j's residues
+# e.g. if delta(res) >= 3, there are 2 or more residues between atoms i's and j's residues
 my $DELTA_RESIDUES = 2;
 
-my $Project_Number = $ARGV[0] or die "\n";    #TODO:
-$MAX_DISTANCE   = $ARGV[1] or die "\n";       #TODO:
-$DELTA_RESIDUES = $ARGV[2] or die "\n";       #TODO:
+my $Project_Number = $ARGV[0] or die "A project number must be specified\n";
+$MAX_DISTANCE   = $ARGV[1] or die "\n";    #TODO:
+$DELTA_RESIDUES = $ARGV[2] or die "\n";    #TODO:
 
-my $work = `pwd`;
-chomp $work;
-print STDOUT "$work\n";
-
+my $Cwd = getcwd();
 my $Total_Frames;
-do_work();
+do_work($Cwd);
 
 print STDOUT "Total Frames = $Total_Frames ... ";
 print STDOUT "Done!\n";
 
 sub do_work {
-    my $run_path  = $work . "/PROJ" . $Project_Number . "\/RUN" . "\*\/";
-    my $run_count = `ls -d $run_path | wc | awk '{print \$1}'`;
-    chomp $run_count;
+    my ($cwd) = @_;
+    my $project_path = "$cwd/PROJ$Project_Number";
+    my $run_count = scalar(get_dirs($project_path, '^RUN\d+$'));
 
-    for (my $run_number = 0 ; $run_number < int($run_count) ; $run_number++) {
-        my $clonepath   = "${work}/PROJ${Project_Number}/RUN${run_number}/CLONE*/";
-        my $clone_count = `ls -d $clonepath | wc | awk '{print \$1}'`;
-        chomp($clone_count);
+    for (my $run_number = 0 ; $run_number < $run_count ; $run_number++) {
+        my $run_path = "$project_path/RUN$run_number";
+        if (not -d $run_path) { next; } #TODO: print info?
 
-        for (my $clone_number = 0 ; $clone_number < int($clone_count) ; $clone_number++) {
-            my $file_path  = "${work}/PROJ${Project_Number}/RUN${run_number}/CLONE${clone_number}/p${Project_Number}*.pdb";
-            my $file_count = `ls $file_path | wc | awk '{print \$1}'`;
-            chomp $file_count;
+        my $clone_count = scalar(get_dirs($run_path, '^CLONE\d+$'));
+        for (my $clone_number = 0 ; $clone_number < $clone_count ; $clone_number++) {
+            my $clone_path = "$run_path/CLONE$clone_number";
+            if (not -d $clone_path) { next; } #TODO: print info?
 
-            for (my $frame_number = 0 ; $frame_number < int($file_count) ; $frame_number++) {
-                my $pdb_name = "p${Project_Number}_r${run_number}_c${clone_number}_f${frame_number}.pdb";
-                my $pdb_file = "${work}/PROJ${Project_Number}/RUN${run_number}/CLONE${clone_number}/${pdb_name}";
+            my $pdb_file_count = scalar(get_files($clone_path, "p${Project_Number}_.*\.pdb\$"));
+            for (my $frame_number = 0 ; $frame_number < $pdb_file_count ; $frame_number++) {
+                my $pdb_name   = "p${Project_Number}_r${run_number}_c${clone_number}_f${frame_number}.pdb";
+                my $pdb_values = parse_pdb("$clone_path/$pdb_name");
+                if (not defined $pdb_values || scalar(@$pdb_values) == 0) { next; }    #TODO: print info?
 
-                my $con_name = "p${Project_Number}_r${run_number}_c${clone_number}_f${frame_number}.con";
-                my $con_file = "${work}/PROJ$Project_Number/RUN$run_number/CLONE$clone_number/$con_name";
-
-                calculate_contacts($pdb_file, $con_file);
+                my $con_name = $pdb_name;
+                $con_name =~ s/pdb$/con/;
+                calculate_contacts($pdb_values, "$clone_path/$con_name");
             }
         }
     }
 }
 
-# find native contacts with condition delta residue >=4 and distant between two atoms <=3.0
-# No, this code is finding ALL contacts, not just native contacts ... that's why it's now called a *.con file
-sub calculate_contacts {
-    my ($pdb_file, $con_file) = pop(@_);
+sub get_dirs {
+    my ($root, $match_pattern) = @_;
+    if (not -d $root) { return; }
+    if ($root !~ m/\/$/) { $root .= "/"; }
+
+    opendir(my $ROOT_HANDLE, $root);
+    my @dirs = grep { -d "$root$_" && /$match_pattern/ } readdir($ROOT_HANDLE);
+    closedir($ROOT_HANDLE);
+
+    return @dirs;
+}
+
+sub get_files {
+    my ($root, $match_pattern) = @_;
+    if (not -d $root) { return; }
+    if ($root !~ m/\/$/) { $root .= "/"; }
+
+    opendir(my $ROOT_HANDLE, $root);
+    my @files = grep { -e "$root$_" && /$match_pattern/ } readdir($ROOT_HANDLE);
+    closedir($ROOT_HANDLE);
+
+    return @files;
+}
+
+sub parse_pdb {
+    my ($pdb_file) = @_;
 
     if (not -e $pdb_file) {
-        print STDOUT "ERROR: pdb file $pdb_file does not exist ...\n";
+        print STDOUT "[ERROR] $pdb_file does not exist ...\n";
         return;
     }
 
     my $pdbsize = calc_file_size($pdb_file);
     if ($pdbsize == 0) {
-        print STDOUT "ERROR: pdb file $pdb_file is zero-sized ...\n";
+        print STDOUT "[ERROR]  $pdb_file is zero-sized ...\n";
         return;
     }
 
-    calculate_contacts_for_real($pdb_file, $con_file);
+    print STDOUT "[INFO]  Parsing $pdb_file\n";
+
+    my @pdb_values;
+    open(my $PDB, '<', $pdb_file) or die $!;
+    while (my $line = <$PDB>) {
+        if ($line !~ m/^\s*ATOM/) { next; }
+        chomp(my @pdb_values = split(/\b\s+\b/, $line));
+        push(@pdb_values, \@pdb_values);
+
+    }
+    close($PDB) or die "$pdb_file: $!\n";
+
+    return \@pdb_values;
 }
 
 sub calc_file_size {
-    my ($pdb_file) = @_;
-    my $size = `wc $pdb_file`;
-    chomp $size;
-    my @sizearray = split(/\b\s+\b/, $size);
-    my $pdbsize = $sizearray[0];
-    return $pdbsize;
+    my ($file) = @_;
+    return int(-s $file);
 }
 
-sub calculate_contacts_for_real {
-    my ($pdb_file, $con_file) = @_;
-
-    print STDOUT "Parsing $pdb_file\n";
-    my @data;
-    my $total_rows;
-    open(my $PDB, '<', $pdb_file) or die $!;
-    while (my $line = <$PDB>) {
-        chomp(my @pdb_values = split(/\b\s+\b/, $line));
-        if ($pdb_values[0] ne "ATOM") { next; }
-        push(@data, \@pdb_values);
-        $total_rows++;
-    }
-    close($PDB) || die $!;
-
-    #compare distance between two atoms
-    open(my $W, '>', $con_file) or die "[FATAL]  $con_file: $!\n";
+sub calculate_contacts {
+    my ($pdb_values, $con_file) = @_;
+    my $total_rows = scalar(@$pdb_values);
+    open(my $CON, '>', $con_file) or die "[FATAL]  $con_file: $!\n";
     for (my $i = 0 ; $i < $total_rows ; $i++) {
         for (my $j = $i + 1 ; $j < $total_rows ; $j++) {
-            my $deltaRes = abs($data[$j][4] - $data[$i][4]);
+            my $delta_residues = abs($$pdb_values[$j][4] - $$pdb_values[$i][4]);
+            if ($delta_residues <= $DELTA_RESIDUES) { next; }
 
-            # delta residues >=3 ... No: now we use only > and start at 4, so it's >= 5 now
-            if ($deltaRes <= $DELTA_RESIDUES) { next; }
+            my $deltaX   = $$pdb_values[$j][5] - $$pdb_values[$i][5];
+            my $deltaY   = $$pdb_values[$j][6] - $$pdb_values[$i][6];
+            my $deltaZ   = $$pdb_values[$j][7] - $$pdb_values[$i][7];
+            my $distance = sqrt($deltaX**2 + $deltaY**2 + $deltaZ**2);
 
-            my $deltaX   = $data[$j][5] - $data[$i][5];
-            my $deltaY   = $data[$j][6] - $data[$i][6];
-            my $deltaZ   = $data[$j][7] - $data[$i][7];
-            my $distance = sqrt(($deltaX * $deltaX) + ($deltaY * $deltaY) + ($deltaZ * $deltaZ));
-
-            # only keep it if it's < the desired cutoff ... default at 6.0 A
+            # only keep it if it's < the desired cutoff ... default at 6.0 A (#REVIEW: out-dated value?)
             if ($distance > $MAX_DISTANCE) { next; }
-            print $W $data[$i][1] . "\t" . $data[$i][2] . "\t" . $data[$i][3] . "\t" . $data[$i][4] . "\t\t";
-            print $W $data[$j][1] . "\t" . $data[$j][2] . "\t" . $data[$j][3] . "\t" . $data[$j][4] . "\t\t" . $deltaRes . "\t";
-            printf $W "%7.3f\n", $distance;
+
+            print $CON $$pdb_values[$i][1] . "\t"    # atom number
+              . $$pdb_values[$i][2] . "\t"           # atom name
+              . $$pdb_values[$i][3] . "\t"           # residue name
+              . $$pdb_values[$i][4] . "\t\t";        # residue number
+            print $CON $$pdb_values[$j][1] . "\t"    # atom number
+              . $$pdb_values[$j][2] . "\t"           # atom name
+              . $$pdb_values[$j][3] . "\t"           # residue name
+              . $$pdb_values[$j][4] . "\t\t"         # residue number
+              . $delta_residues . "\t";
+            printf $CON "%7.3f\n", $distance;        # Euclidean distance in Angstroms
         }
     }
-    close($W) or die "[FATAL]  $con_file: $!\n";
+    close($CON) or die "[FATAL]  $con_file: $!\n";
 }
 
 =head1 NAME
 
 fah-cons-calc.pl - #TODO: description
+# find native contacts with condition delta residue >=4 and distant between two atoms <=3.0
+# No, this code is finding ALL contacts, not just native contacts ... that's why it's now called a *.con file
 
 =head1 SYNOPSIS
 
