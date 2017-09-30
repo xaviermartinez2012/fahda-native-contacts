bd3dab6f24f8940eef788d6720e5b5c1eab9d203
Khai Nguyen <khai.nguyen@outlook.com>
2017-09-30 02:36:28 -0700

    BREAKING CHANGE: Redefine delta(res)
    * Add Share::FileUtil module for getting files matching pattern & checking
      if a file is ok

--- all-contacts/cons-make.pl
+++ all-contacts/cons-make.pl
@@ -2,142 +2,133 @@
 
 use strict;
 use warnings;
+
 use Cwd;
+use FindBin qw($Bin);
+use Getopt::Long qw(HelpMessage :config pass_through);
 use Getopt::Long qw(HelpMessage);
+use lib "$Bin/../lib";
+use Share::DirUtil qw(get_dirs);
+use Share::FileUtil qw(get_files);
+use Share::Fahda;
 
 GetOptions("help|h" => sub { print STDOUT HelpMessage() });
 
-# 7.0 A or less atomic seperations will be recorded
-# my $MAX_DISTANCE = 7.0;
+# HISTORICAL:
+## 7.0 A or less atomic seperations will be recorded
+# my $MAX_DISTANCE 7.0;
+## Delta(res) = res(j) - res(i) = the number of residues between atoms i's and j's residues
+## e.g. if delta(res) >= 3, there are 2 or more residues between atoms i's and j's residues
+# my $DELTA_RESIDUES = 2;
 
-# Delta(res) = res(j) - res(i) = the number of residues between atoms i's and j's residues
-# e.g. if delta(res) >= 3, there are 2 or more residues between atoms i's and j's residues
-my $DELTA_RESIDUES = 2;
+# BREAKING CHANGE
+# Delta(res) = res(j) - res(i) - 1 = # of residues between atoms i's & j's residues
+# e.g. if delta(res) >= 2, there are 2 or more residues between atoms i's & j's residues
 
-my $Project_Number = $ARGV[0] or die "A project number must be specified\n";
-$MAX_DISTANCE   = $ARGV[1] or die "\n";    #TODO:
-$DELTA_RESIDUES = $ARGV[2] or die "\n";    #TODO:
+my $Project_Dir        = $ARGV[0] or die "[FATAL]  PROJ* dir must be specified\n";
+my $Max_Distance_In_A  = $ARGV[1] or die "[FATAL]  Max distance value (in Angstroms) must be specified\n";
+my $Min_Delta_Residues = $ARGV[2] or die "[FATAL]  Delta residues values must be specified\n";
 
-my $Cwd = getcwd();
-my $Total_Frames;
-do_work($Cwd);
+$Project_Dir =~ s/\/$//;
+my ($Project_Number) = $Project_Dir =~ /(\d+$)/;
 
-print STDOUT "Total Frames = $Total_Frames ... ";
+my $Output_File = "make_FAH-CONs_$Project_Dir.log";
+open(my $OUT, '>', $Output_File) or die "[FATAL]  $Output_File: $!\n";
+
+do_work("${\getcwd()}/$Project_Dir");
+
+close($Output_File);
 print STDOUT "Done!\n";
 
 sub do_work {
-    my ($cwd) = @_;
-    my $project_path = "$cwd/PROJ$Project_Number";
-    my $run_count = scalar(get_dirs($project_path, '^RUN\d+$'));
+    my ($project_path) = @_;
 
-    for (my $run_number = 0 ; $run_number < $run_count ; $run_number++) {
+    my $max_run_number = Share::Fahda::get_max_dir_number(get_dirs($project_path, '^RUN\d+$'));
+    for (my $run_number = 0 ; $run_number <= $max_run_number ; $run_number++) {
         my $run_path = "$project_path/RUN$run_number";
-        if (not -d $run_path) { next; } #TODO: print info?
+        if (not -d $run_path) {
+            print $OUT "$run_path not found\n";
+            next;
+        }
+        print $OUT "Working on $run_path\n";
 
-        my $clone_count = scalar(get_dirs($run_path, '^CLONE\d+$'));
-        for (my $clone_number = 0 ; $clone_number < $clone_count ; $clone_number++) {
+        my $max_clone_number = Share::Fahda::get_max_dir_number(get_dirs($run_path, '^CLONE\d+$'));
+        for (my $clone_number = 0 ; $clone_number <= $max_clone_number ; $clone_number++) {
             my $clone_path = "$run_path/CLONE$clone_number";
-            if (not -d $clone_path) { next; } #TODO: print info?
+            if (not -d $clone_path) {
+                print $OUT "$clone_path not found\n";
+                next;
+            }
+            print $OUT "Working on $clone_path\n";
 
             my $pdb_file_count = scalar(get_files($clone_path, "p${Project_Number}_.*\.pdb\$"));
             for (my $frame_number = 0 ; $frame_number < $pdb_file_count ; $frame_number++) {
-                my $pdb_name   = "p${Project_Number}_r${run_number}_c${clone_number}_f${frame_number}.pdb";
-                my $pdb_values = parse_pdb("$clone_path/$pdb_name");
-                if (not defined $pdb_values || scalar(@$pdb_values) == 0) { next; }    #TODO: print info?
+                my $pdb_filename = "p${Project_Number}_r${run_number}_c${clone_number}_f${frame_number}.pdb";
+                my $pdb_path     = "$clone_path/$pdb_filename";
+                if (!Share::FileUtil::file_ok($pdb_path)) {
+                    print $OUT "${Share::FileUtil::File_Ok_Message}\n";
+                    next;
+                }
 
-                my $con_name = $pdb_name;
-                $con_name =~ s/pdb$/con/;
-                calculate_contacts($pdb_values, "$clone_path/$con_name");
+                print $OUT "Parsing $pdb_path\n";
+                my $pdb_values = parse_pdb("$pdb_path");
+                if (not defined $pdb_values || scalar(@$pdb_values) == 0) {
+                    print $OUT "No atom info found in $pdb_path\n";
+                    next;
+                }
+                my $con_filename = $pdb_filename;
+                $con_filename =~ s/pdb$/con/;
+                calculate_contacts($pdb_values, "$clone_path/$con_filename");
             }
         }
     }
 }
 
-sub get_dirs {
-    my ($root, $match_pattern) = @_;
-    if (not -d $root) { return; }
-    if ($root !~ m/\/$/) { $root .= "/"; }
-
-    opendir(my $ROOT_HANDLE, $root);
-    my @dirs = grep { -d "$root$_" && /$match_pattern/ } readdir($ROOT_HANDLE);
-    closedir($ROOT_HANDLE);
-
-    return @dirs;
-}
-
-sub get_files {
-    my ($root, $match_pattern) = @_;
-    if (not -d $root) { return; }
-    if ($root !~ m/\/$/) { $root .= "/"; }
-
-    opendir(my $ROOT_HANDLE, $root);
-    my @files = grep { -e "$root$_" && /$match_pattern/ } readdir($ROOT_HANDLE);
-    closedir($ROOT_HANDLE);
-
-    return @files;
-}
-
 sub parse_pdb {
     my ($pdb_file) = @_;
 
-    if (not -e $pdb_file) {
-        print STDOUT "[ERROR] $pdb_file does not exist ...\n";
-        return;
-    }
-
-    my $pdbsize = calc_file_size($pdb_file);
-    if ($pdbsize == 0) {
-        print STDOUT "[ERROR]  $pdb_file is zero-sized ...\n";
-        return;
-    }
-
-    print STDOUT "[INFO]  Parsing $pdb_file\n";
-
-    my @pdb_values;
+    my @pdb_values = ();
     open(my $PDB, '<', $pdb_file) or die $!;
     while (my $line = <$PDB>) {
+        $line =~ s/^\s*//;
         if ($line !~ m/^\s*ATOM/) { next; }
-        chomp(my @pdb_values = split(/\b\s+\b/, $line));
-        push(@pdb_values, \@pdb_values);
-
+        chomp(my @fields = split(/\s+/, $line));
+        push(@pdb_values, \@fields);
     }
     close($PDB) or die "$pdb_file: $!\n";
 
     return \@pdb_values;
 }
 
-sub calc_file_size {
-    my ($file) = @_;
-    return int(-s $file);
-}
-
 sub calculate_contacts {
     my ($pdb_values, $con_file) = @_;
+
     my $total_rows = scalar(@$pdb_values);
+
     open(my $CON, '>', $con_file) or die "[FATAL]  $con_file: $!\n";
     for (my $i = 0 ; $i < $total_rows ; $i++) {
         for (my $j = $i + 1 ; $j < $total_rows ; $j++) {
-            my $delta_residues = abs($$pdb_values[$j][4] - $$pdb_values[$i][4]);
-            if ($delta_residues <= $DELTA_RESIDUES) { next; }
+            my $delta_residues = abs($$pdb_values[$j][4] - $$pdb_values[$i][4]) - 1;
+            if ($delta_residues < $Min_Delta_Residues) { next; }
 
             my $deltaX   = $$pdb_values[$j][5] - $$pdb_values[$i][5];
             my $deltaY   = $$pdb_values[$j][6] - $$pdb_values[$i][6];
             my $deltaZ   = $$pdb_values[$j][7] - $$pdb_values[$i][7];
             my $distance = sqrt($deltaX**2 + $deltaY**2 + $deltaZ**2);
 
-            # only keep it if it's < the desired cutoff ... default at 6.0 A (#REVIEW: out-dated value?)
-            if ($distance > $MAX_DISTANCE) { next; }
+            # only keep it if it's <= the desired cutoff ...
+            # default at 6.0 A (#REVIEW: out-dated value?)
+            if ($distance > $Max_Distance_In_A) { next; }
 
-            print $CON $$pdb_values[$i][1] . "\t"    # atom number
-              . $$pdb_values[$i][2] . "\t"           # atom name
-              . $$pdb_values[$i][3] . "\t"           # residue name
-              . $$pdb_values[$i][4] . "\t\t";        # residue number
-            print $CON $$pdb_values[$j][1] . "\t"    # atom number
-              . $$pdb_values[$j][2] . "\t"           # atom name
-              . $$pdb_values[$j][3] . "\t"           # residue name
-              . $$pdb_values[$j][4] . "\t\t"         # residue number
-              . $delta_residues . "\t";
-            printf $CON "%7.3f\n", $distance;        # Euclidean distance in Angstroms
+            printf $CON "%5d\t%5s\t%3s\t%4d\t", $$pdb_values[$i][1],    # atom number
+              $$pdb_values[$i][2],                                      # atom name
+              $$pdb_values[$i][3],                                      # residue name
+              $$pdb_values[$i][4];                                      # residue number
+            printf $CON "%5d\t%5s\t%3s\t%4d\t", $$pdb_values[$j][1],    # atom number
+              $$pdb_values[$j][2],                                      # atom name
+              $$pdb_values[$j][3],                                      # residue name
+              $$pdb_values[$j][4];                                      # residue number
+            printf $CON "%4d\t%7.3f\n", $delta_residues, $distance;
         }
     }
     close($CON) or die "[FATAL]  $con_file: $!\n";
@@ -145,15 +136,14 @@ sub calculate_contacts {
 
 =head1 NAME
 
-fah-cons-calc.pl - #TODO: description
-# find native contacts with condition delta residue >=4 and distant between two atoms <=3.0
-# No, this code is finding ALL contacts, not just native contacts ... that's why it's now called a *.con file
+cons-make.pl - find all atom-atom contacts
 
 =head1 SYNOPSIS
 
-./fah-cons-calc.pl  PROJ  <max_atomic_distance (7.0 A)>  <min residue seperation (2 res)> >& stderr.log &
+cons-make.pl <project_dir> <max_atomic_distance> <min_residue_separation>
 
-Prints out individual con files but does NOT concatenate them into a single contacts-log file,
-which is done by fah-cons-join.pl
+Find atom-to-atom contacts where delta residue >= <min_residue_separation> and
+atomic distance <= <max_atomic_distance>. Prints out to individual con files,
+run cons-join.pl to concatenate them.
 
 =cut
