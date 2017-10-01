de74f960f5c1de5356d17fa972236e81e9cfb467
Khai Nguyen <khai.nguyen@outlook.com>
2017-10-01 15:20:40 -0700

    Code refactoring
    * Remove unnecessary variables
    * Rename variables to better show meaning/intentions
    * Use Getopt::Long for showing usage
    * Add pod

--- native-contacts/1-find-native-contacts-from-native-sims.pl
+++ native-contacts/1-find-native-contacts-from-native-sims.pl
@@ -1,105 +1,73 @@
 #!/usr/bin/perl
 
-# ------------------------------------------------------------------------------
-# Collecting all contacts from simulations that were determined to be native
-# Originally written by Amethyst Radcliffe on 10/01/2013
-# ------------------------------------------------------------------------------
-
 use strict;
+use warnings;
 
-# immediately flush anything in buffer to output stream
-use FileHandle;
-STDOUT->autoflush(1);
+use Getopt::Long qw(HelpMessage :config pass_through);
 
-# USAGE & GETTING INPUT INFORMATION INTO APPROPRIATE VARIABLES
-# ------------------------------------------------------------------------------
-$usage      = "\$perl script.pl  [native-sims-list.txt]  [all-contact-data.txt]  [output.txt]\n";
-$natSimFile = $ARGV[0] or die "$usage\n";
-$allConFile = $ARGV[1] or die "$usage\n";
-$outFile    = $ARGV[2] or die "$usage\n";
+GetOptions("help|h" => sub { print HelpMessage(0) });
 
-if ($ARGV[0] eq "-h" || $ARGV[0] eq "--help") {
-    print $usage;
-    exit();
-}
+my $Native_Sims_File = $ARGV[0] or die "[FATAL]  A list of proj/run/clone of native sims must be specified\n";
+my $Joined_Cons_File = $ARGV[1] or die "[FATAL]  A contact .con file must be specified\n";
+my $Output_File      = $ARGV[2] or die "[FATAL]  An output filename must be specified\n";
 
-@Native_Simulations      = ();
-
-# READING IN & STORING THE LIST CONTAINING NATIVE STATE SIMULATIONS
-# ------------------------------------------------------------------------------
-open(my $NSFILE, '<', $natSimFile) or die "ERROR: Cannot open $natSimFile: $!\n";
-print "Reading $natSimFile...\n";
-
-while (my $line = <$NSFILE>) {
-    if ($line =~ m/#/) { next; }    # ignore comments/header in file
-    chomp($line);
-
-    # @values should have (0) project, (1) run, (2) clone, (3) total time (ps)
-    my @values = split(/\s+/, $line);
-    push(@Native_Simulations, "p$values[0]_r$values[1]_c$values[2]");
-}
-
-close($NSFILE) or die "ERROR: $natSimFile: $!\n";
+print STDOUT "[INFO]  Parsing $Native_Sims_File\n";
+my $Native_Sims = parse_native_sims_file($Native_Sims_File);
 
 #  Adding a check to make sure that the native simulations data is useable
-$NSindex = scalar(@Native_Simulations);
+my $NSindex = scalar(keys %$Native_Sims);
 if ($NSindex == 0) {
-    print STDOUT "FATAL ERROR: There are no native simulations being used, ";
-    print STDOUT "make sure native simulation file $natSimFile exists and is not empty.\n";
-    exit;
+    die "[FATAL]  There are no native simulations being used, "
+      . "make sure native simulation file $Native_Sims_File exists and is not empty.\n";
 }
 else {
-    print "Native sim file $natSimFile contains usable data. ";
+    print STDOUT "[INFO]  Native sim file $Native_Sims_File contains usable data";
 }
 
-# SETTING LOOP VARIABLES
-# ------------------------------------------------------------------------------
-$printLine     = 0;
-$shouldWePrint = 0;
-@ProjRunClone  = ();
-$projIndicator = "";    # to get the first letter in the time stamp
-                        # (from the concatenated data file)
-
-# PROCESS LINES FROM THE ALL CONTACTS DATA FILE
-# ------------------------------------------------------------------------------
-open(my $OUT, '>>', $outFile) || die "ERROR: Cannot open $outFile: $!\n";
-open(my $CONFILE, "<", $allConFile) or die "ERROR: Cannot open $allConFile: $!\n";
-
-LBL: while (my $line = <$CONFILE>) {
+my $Is_From_Native_Sim = 0;
+open(my $OUT,     '>', $Output_File)      or die "[FATAL]  $Output_File: $!\n";
+open(my $CONFILE, '<', $Joined_Cons_File) or die "[FATAL]  $Joined_Cons_File: $!\n";
+while (my $line = <$CONFILE>) {
     chomp($line);
-    $printLine = $line;
 
-    # get the first letter to check if this line is indicating a set of contacts for a new time stamp
-    $projIndicator = substr($line, 0, 1);
-
-    #####  Firstly, it checks to see if the line should be printed
-    if ($shouldWePrint == 1 and $projIndicator ne "p") {
-        print $OUT "$printLine\n";
+    if ($Is_From_Native_Sim and $line !~ /^p/) {
+        print $OUT "$line\n";
     }
 
-    # COMPARE & PRINT NATIVE SIMS CONTACTS
-    # check the current line is that of a time stamp (e.g. "p1796_r0_c0_f0.con");
-    # it is a timestamp if the first letter of the string is "p"
-    if ($projIndicator ne "p") { next LBL; }
+    # Skip if current line is not a timestamp (e.g. "p1796_r0_c0_f0.con")
+    if ($line !~ /^p/) { next; }
 
-    @ProjRunClone = split(/_f/, $line);    #split away frame number
-
-    for ($i = 0 ; $i < $NSindex ; $i++) {
-        if (    ($ProjRunClone[0] eq $Native_Simulations[$i])
-            and ($ProjRunClone[1] ne "0.con"))
-        {
-            $shouldWePrint = 1;
-            print "Match found: 1: $ProjRunClone[0]_f$ProjRunClone[1], 2: $Native_Simulations[$i]\n";
-            next LBL;
-        }
-        else {
-            $shouldWePrint = 0;
-        }
-    }
+    my ($prc, $frame_number) = split(/_f/, $line);
+    $Is_From_Native_Sim = ($$Native_Sims{$prc} == 1 and $frame_number ne "0.con") ? 1 : 0;
 }
-close $CONFILE;
-close $OUT;
+close($CONFILE);
+close($OUT);
 
-print "Sorting $outFile...\n";
-system("sort $outFile >> $outFile_sorted.txt");
+#REVIEW: do we need to sort?
+# print STDOUT "[INFO]  Sorting $Output_File...\n";
+# `sort $Output_File > $Output_File_sorted.txt`;
 print "Done!\n";
+
+sub parse_native_sims_file {
+    my ($native_sims_file) = @_;
+    my %native_sims = {};
+    open(my $NSFILE, '<', $native_sims_file) or die "[FATAL]  $native_sims_file: $!\n";
+    while (my $line = <$NSFILE>) {
+        if ($line =~ m/^#/) { next; }
+        chomp(my @fields = split(/\b\s+\b/, $line));
+        my ($project_number, $run_number, $clone_number) = @fields;
+        $native_sims{"p${project_number}_r${run_number}_c${clone_number}"} = 1;
+    }
+    close($NSFILE);
+    return \%native_sims;
+}
+
+=head1 NAME
+
+find-native-sims-contacts.pl - collect all contacts from native simulations
+
+=head1 SYNOPSIS
+
+find-native-sims-contacts.pl  <native_sims.txt> <joined_cons.con> <output.txt>
+
+=cut
